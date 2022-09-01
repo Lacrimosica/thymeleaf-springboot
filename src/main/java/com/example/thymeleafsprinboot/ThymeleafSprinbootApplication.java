f367fc4fa3623c7ce9d38a5cb64232d05c522498
Lacrimosica <m.gooudarzi77@hotmail.com>
2022-09-01 15:05:52 +0200

    init

---
+++ src/main/java/com/example/thymeleafsprinboot/ThymeleafSprinbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafsprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSprinbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSprinbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleafsprinboot/ThymeleafSprinbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafsprinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSprinbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
